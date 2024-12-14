0593b51c5536055cb7e2dabd3382952d3a85bff2
1amAzrael <sangambkh21@gmail.com>
2024-12-14 23:24:31 +0545

    header

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Home from './Pages/Home'
 import Signup from './Pages/Signup'
 import Signin from './Pages/Signin'
 import Profile from './Pages/Profile'
+import Header from './Component/Header'
+import About from './Pages/About'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
        
       
     </Routes>
+    
     </BrowserRouter>
   )
 }
