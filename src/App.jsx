b1e7e318d99b087bd4eed7f07617aacec9d116fe
1amAzrael <sangambkh21@gmail.com>
2024-12-14 22:49:49 +0545

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signup from './Pages/Signup'
+import Signin from './Pages/Signin'
+import Profile from './Pages/Profile'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/home" element={<Home />} />
+       
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
 
