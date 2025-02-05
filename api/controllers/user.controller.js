5a14df9c4e389507c655b57b3465a6fc2f038dc2
1amAzrael <sangambkh21@gmail.com>
2025-02-05 22:46:29 +0545

    user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,29 @@
+
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.send('Hello World')
-}
+};
+
+export const updateUser = async (req, res) => {
+    if(req.user.id !== req.params.id) return res.status(403).json("You can only update your account!");
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const updatedUser =await User.findByIdAndUpdate(req.params.id, { $set:
+            {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                photoURL: req.body.photoURL
+            }
+        }, { new: true });
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+
 dotenv.config()
 
 
@@ -14,6 +16,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
